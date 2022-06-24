70b0cb87870546c93c3d5fb556b3689d2765cdff
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-06-24 22:23:50 +0300

    fear: errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,8 +1,10 @@
 const Card = require('../models/card');
 
+const ERROR_CODE = 400;
+
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.status(200).send(cards))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
@@ -11,34 +13,71 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE).send({ message: 'Данные введены неверно' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.delCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(() => {
+      const error = new Error('Карточка не найдена');
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.statusCode === 404 || err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log('Id: ', req.params.cardId);
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
-  ).then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  ).orFail(() => {
+    const error = new Error('Карточка не найдена');
+    error.statusCode = 404;
+    throw error;
+  })
+    .then((likes) => res.status(200).send(likes))
+    .catch((err) => {
+      if (err.statusCode === 404 || err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log('Id: ', req.params.cardId);
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
-  ).then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  ).orFail(() => {
+    const error = new Error('Карточка не найдена');
+    error.statusCode = 404;
+    throw error;
+  })
+    .then((likes) => res.status(200).send(likes))
+    .catch((err) => {
+      if (err.statusCode === 404 || err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,44 @@
 const User = require('../models/user');
 
+const ERROR_CODE = 400;
+
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.status(200).send(users))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
 module.exports.getUserbyId = (req, res) => {
   User.find({ _id: req.params.userId })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(() => {
+      const error = new Error('Пользователь не найден');
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.statusCode === 404 || err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(201).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE).send({ message: 'Данные введены неверно' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.editUserProfile = (req, res) => {
@@ -26,10 +48,25 @@ module.exports.editUserProfile = (req, res) => {
     { name, about },
     {
       new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true,
     },
-  )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  ).orFail(() => {
+    const error = new Error('Пользователь не найден');
+    error.statusCode = 404;
+    throw error;
+  })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE).send({ message: 'Данные введены неверно' });
+        return;
+      }
+      if (err.statusCode === 404 || err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.editUserAvatar = (req, res) => {
@@ -39,8 +76,23 @@ module.exports.editUserAvatar = (req, res) => {
     { avatar },
     {
       new: true,
+      runValidators: true,
     },
-  )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  ).orFail(() => {
+    const error = new Error('Пользователь не найден');
+    error.statusCode = 404;
+    throw error;
+  })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE).send({ message: 'Данные введены неверно' });
+        return;
+      }
+      if (err.statusCode === 404 || err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
