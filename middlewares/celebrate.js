43545d0cc68e02123176d1f044a2a57ed0902791
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-07-08 12:45:22 +0300

    fix: joi regex validation

--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -1,10 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
 
+const regex = /https?:\/\/(www\.)?[\w.\-~:/?#[\]@!$&'()*+,;=]+#?/i;
+
 module.exports.usersJoi = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(regex),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
@@ -27,10 +29,9 @@ module.exports.userInfoJoi = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(regex),
   }),
 });
-
 module.exports.cardsJoi = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
