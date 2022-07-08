e93c124cbef775c31bf1a1a8a6b1852b23e53ebe
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-07-08 13:22:06 +0300

    fix: error codes

--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -35,7 +35,7 @@ module.exports.userInfoJoi = celebrate({
 module.exports.cardsJoi = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(regex),
   }),
 });
 
